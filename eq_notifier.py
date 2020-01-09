bce3b4a8e71eaff5bddbabacf87a05b82edc168b
alexandru-grajdeanu <grajdeanu.alex@gmail.com>
2020-01-09 18:37:42 +0200

    Restructuring

--- eq_notifier.py
+++ eq_notifier.py
@@ -1,10 +1,20 @@
+"""
+This projects aims to send an SMS to a specific number using Twilio
+if an earthquake with magnitude > 4 is going to occur (depending on
+your location, this can warn you (best case scenario within 25-30
+seconds before you feel the earthquake wave).
+"""
+
+import argparse
 import datetime
 import json
 import re
-import requests
 import time
+from functools import partial
 
+import requests
 from lxml.html import fromstring
+from twilio.base.exceptions import TwilioRestException
 from twilio.rest import Client
 
 
@@ -12,92 +22,130 @@ BASE_URL = 'http://alerta.infp.ro'
 DATA_URL = f'{BASE_URL}/server.php'
 
 
-class EarthquakeData:
-    def __init__(self):
-        self.session = requests.Session()
+def command_line_parser():
+    """
+    Simple command line parser function.
+    """
 
-    @staticmethod
-    def get_secrets():
-        """
-        Get secrets from `credentials.json` file. The file looks like this:
+    parser = argparse.ArgumentParser(description='Earthquake Listener')
+    parser.add_argument(
+        '-t', '--twilio',
+        nargs='?',
+        type=credentials,
+        const='credentials.json',
+        help='Path to the credentials file'
+    )
+    parser.add_argument(
+        '-d', '--delay',
+        type=float,
+        default=1.0,
+        help='Delay in seconds between two calls to the earthquake API'
+    )
+    return parser
+
+
+def credentials(filepath: str) -> dict:
+    """Return secrets from `filepath` file as a dict.
+
+    The file looks like this:
 
         {
-          "TWILIO_ACCOUNT_SID": "Your twilio account sid",
+          "TWILIO_ACCOUNT_SID": "Your twilio account SID",
           "TWILIO_AUTH_TOKEN": "Your twilio account auth token",
           "FROM": "The number from which you'll receive the alert",
           "TO": "The number the message is sent to"
         }
+
+
+    Args:
+        filepath (str): Path to credentials JSON file.
+
+    Returns:
+        dict: The return value.
+    """
+
+    with open(filepath) as credentials_file:
+        credentials_data = json.load(credentials_file)
+
+    if not credentials_data:
+        raise ValueError('Credentials file should not be empty.')
+
+    return credentials_data
+
+
+def get_earthquake_data() -> dict:
+    """Get earthquake data from `DATA_URL`.
+
+    Returns:
+        dict: A dict containing the following data:
+              {
+                'mag': '0.1',
+                'heart': '2020-01-04 13:30:04 HEARTBEAT',
+                'sec': '30',
+                'key': 'NjY2NDYyMzAzNjMwNjM2MzM1Mz...=='
+              }
+    """
+
+    session = requests.Session()
+    with session as page_session:
+        html_page = page_session.get(BASE_URL).content
+        html_script = fromstring(html_page).xpath('//script[contains(., "source")]/text()')[0]
+        key = {
+            'keyto': re.search(
+                r"var source = new EventSource\('server\.php\?keyto=(.*)'\);", html_script
+            ).group(1)
+        }
+        earthquake_data = page_session.get(f'{DATA_URL}', params=key).content
+        earthquake_data = earthquake_data.decode('utf8').replace("data", '"data"').strip()
+        return json.loads(f'{{{earthquake_data}}}')
+
+
+def send_message(twilio_client, send_to, sent_from) -> None:
+    """Send a message via Twilio if the magnitude of an earthquake
+    is bigger than 4.
+    """
+
+    data = get_earthquake_data().get('data')
+    eq_magnitude = data.get('mag')
+
+    if float(eq_magnitude) >= 4:
+        body = f"""ATTENTION!!!
+
+        Earthquake with magnitude: {eq_magnitude} 
+        at {datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')}!
         """
 
-        with open('credentials.json') as credentials_file:
-            data = json.load(credentials_file)
-        return data
-
-    def get_eq_data(self):
-        """
-        Get earthquake information from `http://alerta.infp.ro` as a string.
-        """
-
-        with self.session as session:
-            try:
-                html_page = session.get(BASE_URL).content
-            except requests.exceptions.ConnectionError:
-                time.sleep(10)
-                html_page = session.get(BASE_URL).content
-
-            html_script = fromstring(html_page).xpath('//script[contains(., "source")]/text()')[0]
-            key = {
-                'keyto': re.search(
-                    r"var source = new EventSource\('server\.php\?keyto=(.*)'\);", html_script
-                ).group(1)
-            }
-            return session.get(f'{DATA_URL}', params=key).content
-
-    def transform_eq_data(self):
-        """
-        Convert eq data to a dictionary.
-        """
-
-        data = self.get_eq_data().decode('utf8')
-        data = data.replace("data", '"data"').strip()
-        return json.loads(f'{{{data}}}')
-
-    def send_message(self):
-        twilio_client = Client(
-            self.get_secrets().get('TWILIO_ACCOUNT_SID'),
-            self.get_secrets().get('TWILIO_AUTH_TOKEN')
-        )
-
-        data = self.transform_eq_data().get('data')
-        eq_magnitude = data.get('mag')
-
-        if float(eq_magnitude) >= 4:
-            body = f"""ATTENTION!!!
-            
-            Earthquake with magnitude: {eq_magnitude} 
-            at {datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')}!
-            """
-            try:
-                twilio_client.messages.create(
-                    body=body, from_=self.get_secrets().get('FROM'), to=self.get_secrets().get('TO')
-                )
-            except Exception as e:
-                print(f'Twilio API error: {e}')
-
-
-def main():
-    while True:
-        eq = EarthquakeData()
         try:
-            eq.send_message()
-            time.sleep(15)
-        except TypeError as e:
-            print(e)
-            continue
+            twilio_client.messages.create(body=body, from_=sent_from, to=send_to)
+        except TwilioRestException as error:
+            print(f'Twilio API error: {error}')
+    else:
+        print('No need to worry. YET!')
+
+
+def main(credentials_data=None, delay=1.0) -> None:
+    """Main entry to the program."""
+
+    if credentials_data is None:
+        action = get_earthquake_data
+    else:
+        twilio_client = Client(
+            credentials_data['TWILIO_ACCOUNT_SID'],
+            credentials_data['TWILIO_AUTH_TOKEN']
+        )
+        sender = credentials_data['FROM']
+        receiver = credentials_data['TO']
+        action = partial(send_message, twilio_client, receiver, sender)
+
+    while True:
+        try:
+            action()
+            time.sleep(delay)
+        except KeyboardInterrupt:
+            print('Closing the program...')
+            break
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Closing...')
+    ARGS = command_line_parser().parse_args()
+    main(ARGS.twilio, ARGS.delay)
---
+++ exceptions.py
@@ -0,0 +1,10 @@
+"""
+Main module for all the exceptions across project.
+"""
+
+
+class EmptyCredentialsFile(Exception):
+    """
+    Exception to be raised when credentials file is empty and valid
+    """
+    pass
