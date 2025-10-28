1cd9779d09a6f8a1709b27fd8513a49a66189896
Alexandru Grajdeanu <grajdeanu.alex@gmail.com>
2025-10-28 19:26:46 +0200

    remove exception

--- exceptions.py
+++ exceptions.py
@@ -7,9 +7,3 @@ class EmptyCredentialsFile(Exception):
     """
     Exception to be raised when credentials file is empty and valid.
     """
-
-
-class VeryNewException(Exception):
-    """
-    Very new exception.
-    """
